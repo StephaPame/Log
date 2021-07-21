1420c361bb0bfe80c6bcd3cf86eba35c7e7026c1
StephaPame <stephapamett@gmail.com>
2021-07-21 12:37:58 -0500

    log

---
+++ ionic-firebase-email-authentication/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBu-NDIr-cFqu3gc7xSwBLhidQFZ5TfoCI",
+    authDomain: "logino-166dc.firebaseapp.com",
+    projectId: "logino-166dc",
+    storageBucket: "logino-166dc.appspot.com",
+    messagingSenderId: "918138964590",
+    appId: "1:918138964590:web:9b979e5d8a03d60d96747f"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
